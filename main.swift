2d8f7282e423f2321c0c402ecd0266d4856ec2d7
20141105048 <l20141105048@King.local>
2016-06-21 11:09:38 +0800

    第二次提交
    用swift语句，实现20个数从小到大排序。首先定义一个包含20个元素的数组，采用冒泡循环的算法，将20个数组进行排序，也就是采用2个数排序，将
    错误顺序调换，知道最小数到达首位，排序完成再输出即可

--- main.swift
+++ main.swift
@@ -8,22 +8,23 @@
 
 import Foundation
 
-var number=[9,8,5,6,7,10,13,3,1,2,15,17,16,14,18,19,20,44,33,22]
-for(var i=0;i<number.count-1;i++){
-    for(var j=0;j<number.count-1-i;j++){
+var a=[9,8,5,6,7,10,13,3,1,2,15,17,16,14,18,19,20,44,33,22]
+
+for i in 0..<a.count-1{
+    for j in 0..<a.count-1-i{
         
-        if(number[j]>number[j+1]){
+        if(a[j]>a[j+1]){
             var temp=0
-            temp=number[j]
-            number[j]=number[j+1]
-            number[j+1]=temp
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
         }
     }
-        
+    
         
 
 }
-for item in number{
+for item in a{
 
     print(item)
 }
